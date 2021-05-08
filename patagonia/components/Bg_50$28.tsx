00b7f422536e1d755079c247a93073f4a34372ed
Soichi Takamura <thepiglovesyou@gmail.com>
2021-05-08 19:29:40 +0900

    Updates

---
+++ patagonia/components/Bg_50$28.tsx
@@ -0,0 +1,39 @@
+import React, { FC, CSSProperties } from "react";
+export const Bg_50$28: FC<{
+  style: CSSProperties,
+}> = (props) => {
+  return (
+    <div
+      data-fid="50:28"
+      style={{
+        ...{
+          position: "absolute",
+          boxSizing: "border-box",
+          backgroundColor: "rgba(0, 0, 0, 0)",
+          backgroundImage:
+            "url(https://s3-alpha-sig.figma.com/img/c73f/97be/f3d3bc2931ffab08c3df439b1392ed07?Expires=1621209600&Signature=Sg6iegIk0A5vSyD9GLSbVWZ3SfbcMUiNxFjWkCzOG5cXoTgZkkz-0lZ3FK~1QdTdQs~WyMu1xwkP1dqEY2WuVfqDp0PZsUPYtmve7KPsiEQ6FW197YD7HduV-8hZ30iQP3K7FozyD0p6DCFISctAu4xO1oARF7e4imdBG2AjhCxKdLm8U~RUblGNFUqnr2rmqDAOf-quDbz6pL65dEXLfKF95XtMyzA2cjqwiwK1AEXzIHhYUL2qo6Xcl~~5vq79vCXaSyc3bKbX0yP-tB7yYZ-dvegrOE9ujGabP0r7jJ6asTZmazObsrkeTUTD28UDBPRl2XMTXPhkVcR~iH-xGw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA)",
+          backgroundSize: "cover",
+        },
+        ...props.style,
+      }}
+      data-fname="bg"
+    >
+      <div
+        data-fid="50:29"
+        style={{
+          zIndex: 0,
+          position: "absolute",
+          boxSizing: "border-box",
+          top: 0,
+          height: 1080,
+          left: 0,
+          width: 1440,
+          backgroundImage:
+            "url(https://s3-alpha-sig.figma.com/img/c73f/97be/f3d3bc2931ffab08c3df439b1392ed07?Expires=1621209600&Signature=Sg6iegIk0A5vSyD9GLSbVWZ3SfbcMUiNxFjWkCzOG5cXoTgZkkz-0lZ3FK~1QdTdQs~WyMu1xwkP1dqEY2WuVfqDp0PZsUPYtmve7KPsiEQ6FW197YD7HduV-8hZ30iQP3K7FozyD0p6DCFISctAu4xO1oARF7e4imdBG2AjhCxKdLm8U~RUblGNFUqnr2rmqDAOf-quDbz6pL65dEXLfKF95XtMyzA2cjqwiwK1AEXzIHhYUL2qo6Xcl~~5vq79vCXaSyc3bKbX0yP-tB7yYZ-dvegrOE9ujGabP0r7jJ6asTZmazObsrkeTUTD28UDBPRl2XMTXPhkVcR~iH-xGw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA)",
+          backgroundSize: "cover",
+        }}
+        data-fname="IMG_1564 2 1"
+      ></div>
+    </div>
+  );
+};
--- patagonia/components/CarouselContent1_2$44.tsx
+++ patagonia/components/CarouselContent1_2$44.tsx
@@ -11,7 +11,7 @@ export const CarouselContent1_2$44: FC<{
           boxSizing: "border-box",
           backgroundColor: "rgba(0, 0, 0, 0)",
           backgroundImage:
-            "url(https://s3-alpha-sig.figma.com/img/c73f/97be/f3d3bc2931ffab08c3df439b1392ed07?Expires=1621209600&Signature=Sg6iegIk0A5vSyD9GLSbVWZ3SfbcMUiNxFjWkCzOG5cXoTgZkkz-0lZ3FK~1QdTdQs~WyMu1xwkP1dqEY2WuVfqDp0PZsUPYtmve7KPsiEQ6FW197YD7HduV-8hZ30iQP3K7FozyD0p6DCFISctAu4xO1oARF7e4imdBG2AjhCxKdLm8U~RUblGNFUqnr2rmqDAOf-quDbz6pL65dEXLfKF95XtMyzA2cjqwiwK1AEXzIHhYUL2qo6Xcl~~5vq79vCXaSyc3bKbX0yP-tB7yYZ-dvegrOE9ujGabP0r7jJ6asTZmazObsrkeTUTD28UDBPRl2XMTXPhkVcR~iH-xGw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA)",
+            "url(https://s3-alpha-sig.figma.com/img/bc1a/04d9/6e40f86b35e43b1121d9c766114cb456?Expires=1621209600&Signature=AO1Khjh~89aPV-miKwbEjUH4Ne2lPuaTxkdXaKmXz~MZ-OfWZKHNZ9~qGpWLM2FC1yCJTMn4lFwu4xFP42-nsR6QvQfF1EYG9ebg-7UytgaP1O-~fxa3c6GBy9H0lYW260jIoO5KsbJyYyhRUT6D1Xuwh2saDom-mSELRdC-WR4YEtYO8FcchJO5MzsK8d8Vjxg8tIz0DPWAC1QKdz58Ygd5BxQnbF5v86ihqyPKjkB63FAHm-DJ0egUyHCC9eFpHbVwFYmtrBufwyj89yZ1WqcZIq-Dzr~Mt0hv-4OIB7~CC-xyEy-svRZfYf2bwgU1Nt7n7rWnyM-FLYqeZ3s0fw__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA)",
           backgroundSize: "cover",
           borderRadius: "6.75px 6.75px 6.75px 6.75px",
         },
