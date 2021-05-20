fd3357d5166fed9260afb1258d4df881cca9897d
Soichi Takamura <thepiglovesyou@gmail.com>
2021-05-20 14:59:08 +0900

    Updates

---
+++ mini/pages/Home_1$2.tsx
@@ -0,0 +1,70 @@
+import React, { FC, CSSProperties } from "react";
+export const Home_1$2: FC<{
+  style: CSSProperties,
+}> = (props) => {
+  return (
+    <div
+      data-fid="1:2"
+      style={{
+        ...{
+          position: "absolute",
+          boxSizing: "border-box",
+          backgroundColor: "#ffffff",
+        },
+        ...props.style,
+      }}
+      data-fname="Home"
+    >
+      <div
+        data-fid="1:3"
+        style={{
+          zIndex: 0,
+          position: "absolute",
+          boxSizing: "border-box",
+          top: 42,
+          height: 14,
+          left: 71,
+          width: 205,
+          color: "#000000",
+          fontSize: 12,
+          fontWeight: 400,
+          fontFamily: '"Roboto"',
+          fontStyle: "normal",
+          lineHeight: "125%",
+          letterSpacing: "0px",
+          textAlign: "left",
+        }}
+        data-fname="blaaaaaaaaaa aaaaaaaaaaaa aaaaaaa"
+      >
+        <span style={{}} key="end">
+          blaaaaaaaaaa aaaaaaaaaaaa aaaaaaa
+        </span>
+      </div>
+      <div
+        data-fid="1:4"
+        style={{
+          zIndex: 1,
+          position: "absolute",
+          boxSizing: "border-box",
+          top: 68,
+          height: 42,
+          left: 71,
+          width: 96,
+          color: "#000000",
+          fontSize: 12,
+          fontWeight: 400,
+          fontFamily: '"Roboto"',
+          fontStyle: "normal",
+          lineHeight: "125%",
+          letterSpacing: "0px",
+          textAlign: "left",
+        }}
+        data-fname="blaaaaaaaaaa aaaaaaaaaaaa aaaaaaa"
+      >
+        <span style={{}} key="end">
+          blaaaaaaaaaa aaaaaaaaaaaa aaaaaaa
+        </span>
+      </div>
+    </div>
+  );
+};
