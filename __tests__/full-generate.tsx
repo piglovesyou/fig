23f1a700e1a49935577f84656a6aabb044014880
Soichi Takamura <thepiglovesyou@gmail.com>
2021-05-22 22:31:53 +0900

    refactor
    fix ts errors

--- __tests__/full-generate.tsx
+++ __tests__/full-generate.tsx
@@ -12,8 +12,8 @@ const outDir = join(__dirname, '__generated__');
 
 type CaseArg = [name: string, componentName: string, fileKey: string];
 const caseArgs: CaseArg[] = [
-  // ['mini', 'Home_1$2', 'MhB9ljAxaGlIk1IttXa09f'],
-  // ['basic', 'Home_2$2', 'QAIja81RKgYhQnIIJ0h9PJ'],
+  ['mini', 'Home_1$2', 'MhB9ljAxaGlIk1IttXa09f'],
+  ['basic', 'Home_2$2', 'QAIja81RKgYhQnIIJ0h9PJ'],
   ['patagonia', 'Home_1$4', 'pC6EOjjdZpS7PVsPTgjNLL'],
 ];
 
