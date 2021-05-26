d2663c0cf2803511a9d6c686de313dee37aa86ba
Soichi Takamura <thepiglovesyou@gmail.com>
2021-05-26 18:16:38 +0900

    message for no TOKEN

--- src/core/env.ts
+++ src/core/env.ts
@@ -3,6 +3,10 @@ import { config } from 'dotenv';
 config();
 
 export const token = process.env.TOKEN!;
-if (!token) throw new Error('$TOKEN is needed');
+if (!token)
+  throw new Error(`Please provide a Figma access token, and try again.
+https://www.figma.com/developers/api#access-tokens
+
+$ TOKEN=0000 npx fig`);
 
 export const shouldRefresh = Boolean(process.env.DISABLE_CACHE);
