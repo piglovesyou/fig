9fc3a25fa23fb65ea5ae34c37945ecb9884b66bb
Soichi Takamura <thepiglovesyou@gmail.com>
2021-06-01 11:45:20 +0900

    format html

--- src/strategies/react/index.ts
+++ src/strategies/react/index.ts
@@ -83,7 +83,7 @@ class ReactStrategy implements StrategyInterface {
       React.createElement(PageComponent, null)
     );
 
-    return `<html>
+    const html = `<html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 <style>
@@ -107,6 +107,8 @@ body > * {
 </head>
 <body>${pageHtml}</body>
 </html>`;
+
+    return format(html, { parser: 'html' });
   }
 
   appendComponentInstanceElement(
