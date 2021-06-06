ab0391fd88bbce027182bc5aedae448958a1ea1e
Soichi Takamura <thepiglovesyou@gmail.com>
2021-06-06 16:23:07 +0900

    wip: refactor

--- src/gen/gen.ts
+++ src/gen/gen.ts
@@ -6,26 +6,18 @@ import { requestFile } from '../core/api';
 import { FigConfig } from '../core/config';
 import { FigmaFile } from '../types/fig';
 import { ComponentInfo, GenContext } from '../types/gen';
-import { StrategyInterface } from '../types/strategy';
 import { makeGenContext } from './make-gen-context';
 import { processComponent } from './process-component';
 
-// async function registerTsNodeIfPossible() {
-//   try {
-//     const tsNode = await import('ts-node');
-//     tsNode.register({ transpileOnly: true });
-//     return true;
-//   } catch (e) {}
-//   return false;
-// }
-
 export async function processHtml(
-  strategy: StrategyInterface,
   componentInfo: ComponentInfo,
   genContext: GenContext
 ) {
   const { node, name } = componentInfo;
   if (node.type !== 'FRAME') return;
+  const { strategy } = genContext;
+  if (!strategy) throw new Error('Never. Strategy should be instantiated.');
+
   const html = await strategy.renderHtml(componentInfo);
   await writeFile(join(genContext.htmlFullDir, name + '.html'), html);
 }
@@ -40,20 +32,19 @@ export async function gen(
 
     const genContext = await makeGenContext(figmaFile, fileKey, config, cwd);
     const { componentsMap, strategy } = genContext;
+    if (!strategy) throw new Error('Never. Strategy should be instantiated.');
 
-    // Generate components to "components" and "pages"
-    const result = await pMap(componentsMap, async ([, componentInfo]) => {
-      // TODO: Strategy for genContext, not for components. Are you crazy?
+    // Generate components to "./components" and "./pages"
+    await pMap(componentsMap, async ([, componentInfo]) => {
       await processComponent(componentInfo, genContext);
-      return [componentInfo, strategy] as [ComponentInfo, StrategyInterface];
     });
 
     // Generate html to "./public"
     await makeDir(genContext.htmlFullDir);
-    await pMap(result, async ([componentInfo, strategy]) => {
-      await processHtml(strategy, componentInfo, genContext);
+    await pMap(componentsMap, async ([, componentInfo]) => {
+      await processHtml(componentInfo, genContext);
     });
 
-    await strategy?.dispose();
+    await strategy.dispose();
   }
 }
